2dad32c1ed45378af614f2c6765ec0fff4be441d
Yaashicaa <40803626+Yaashicaa@users.noreply.github.com>
2019-07-26 12:36:34 +0530

    Update Config.java

--- DP/BYClothes/app/src/main/java/com/example/byclothes/Config/Config.java
+++ DP/BYClothes/app/src/main/java/com/example/byclothes/Config/Config.java
@@ -2,5 +2,5 @@ package com.example.byclothes.Config;
 
 public class Config
 {
-    public static final String PAYPAL_CLIENT_ID = "AfxYt1LgiafBnr-byUCHYF5qNKUf_yOv6z9-l3fqFOoMyKfxqVCpJmh4O8_54iq8_FLmPAOYgKt2dlz8";
+    public static final String PAYPAL_CLIENT_ID = "your paypal client ID";
 }
